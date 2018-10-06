f53bd7059e865973632610ca7bda6c484d9f50bb
Yana <holoborodkoyana97@gmail.com>
2018-10-06 18:00:12 +0200

    Script to run the clock

---
+++ script.js
@@ -0,0 +1,27 @@
+const HOURHAND = document.querySelector("#hour");
+const MINUTEHAND = document.querySelector("#minute");
+const SECONDHAND = document.querySelector("#second");
+
+var date = new Date();
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+
+let hrPosition = (hr*360/12)+(min*360/60)/12;
+let minPosition = (min*360/60)+(sec*(360/60)/60);
+let secPosition = sec*360/60;
+
+function runTheClock () {
+   
+    // = hrPosition+30/3600
+    hrPosition = hrPosition+(1/120);
+    // = minPosition+(1/60)*6
+    minPosition = minPosition+(1/10);
+    secPosition = secPosition+6;
+
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
+
+var interval = setInterval(runTheClock, 1000);
